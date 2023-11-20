670d17080035df6225bd4402339c9e31b91a54b7
김태환 <appleit1245@naver.com>
2023-11-21 02:11:38 +0900

    front_v1.1 makeatbar axios back-end comp

--- src/components/MakeAtBar.vue
+++ src/components/MakeAtBar.vue
@@ -1,13 +1,37 @@
 <template>
     <div>This is MakeAtBarPage!</div>
+    <button type="button" style="top: 50%; left: 50%;transform: translate(50%, 1000%); position: absolute;" @click="sendQuest()"><span>Ask!</span></button>
+    <input class="dark-input" type="text" value="questionText" placeholder="질문 입력" @input="quest=$event.target.value"/>
 </template>
 
 <script>
 export default {
-    name: "MakeAtBar.vue"
+    name: "MakeAtBar.vue",
+    data(){
+      return{
+          quest:""
+      }
+    },
+    methods:{
+        sendQuest: function(){
+            let question={};
+            question.question=this.quest;
+            console.log(JSON.stringify(question));
+            this.$axios
+                .post("/chat-gpt/question", question, {
+                    headers:{'Content-Type': 'application/json; charset=UTF-8'},
+                })
+                .then(() => {
+                    console.log('success');
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+        }
+    }
 }
 </script>
 
-<style scoped>
+<style>
 
 </style>
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8082',
+        target: 'http://localhost:8081',
         rewrite: (path)=>path.replace(/^\/api/, ""),
         changeOrigin: true,
       },
